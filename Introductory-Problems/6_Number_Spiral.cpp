587848551d0b78b9f7c35ef4fb7d5b1082fd812c
bat-knight <oti.ababil@gmail.com>
2023-11-23 03:00:50 +0600

    progress

--- Introductory-Problems/6_Number_Spiral.cpp
+++ Introductory-Problems/6_Number_Spiral.cpp
@@ -2,5 +2,28 @@
 #define ll long long
 
 int main(){
-    ll 
+    ll t;
+    std::cin>>t;
+    for(ll i=0;i<t;i++){
+        ll y,x;
+        std::cin>>y>>x;
+        if (y>x){
+            if(y%2==0){
+               std::cout<<(y*y)-(x-1)<<'\n'; 
+            }
+            else{
+                std::cout<<((y-1)*(y-1)+1)+(x-1)<<'\n';
+            }
+        }
+        else{
+            if(x%2==0){
+                std::cout<<((x-1)*(x-1)+1)+(y-1)<<'\n';
+
+            }
+            else{
+                std::cout<<(x*x)-(y-1)<<'\n';
+            }
+        }
+    }
+    return 0;
 }
